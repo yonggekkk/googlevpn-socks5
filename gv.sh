50c4f27a384d5b493f9c984233262f7c29cc03c6
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-11-17 16:31:19 +0800

    Update gv.sh

--- gv.sh
+++ gv.sh
@@ -3,15 +3,16 @@ gvinstall(){
 pkg install -y screen
 if [ ! -e gost ]; then
 curl -L -o gost_3.0.0-nightly.20240904_linux_arm64.tar.gz -# --retry 2 --insecure https://raw.githubusercontent.com/yonggekkk/googlevpn-socks5/main/gost_3.0.0-nightly.20240904_linux_arm64.tar.gz
+tar zxvf gost_3.0.0-nightly.20240904_linux_arm64.tar.gz
 fi
 if [ ! -e gost ]; then
 echo "当前网络无法链接github，切换中转下载"
 curl -L -o gost_3.0.0-nightly.20240904_linux_arm64.tar.gz -# --retry 2 --insecure https://ghp.ci/https://raw.githubusercontent.com/yonggekkk/googlevpn-socks5/main/gost_3.0.0-nightly.20240904_linux_arm64.tar.gz
+tar zxvf gost_3.0.0-nightly.20240904_linux_arm64.tar.gz
 fi
 if [ ! -e gost ]; then
 echo "下载失败，请在代理环境下运行脚本" && exit
 fi
-tar zxvf gost_3.0.0-nightly.20240904_linux_arm64.tar.gz
 rm -f gost_3.0.0-nightly.20240904_linux_arm64.tar.gz README* LICENSE* config.yaml
 read -p "设置 Socks5 端口（回车跳过为10000-65535之间的随机端口）：" port
 if [ -z "$port" ]; then
